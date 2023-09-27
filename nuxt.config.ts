04db4bee60bb20e268a8ce395569937e0f2491f0
ChuKuoHua <enjoy12589@gmail.com>
2023-09-27 10:50:29 +0800

    測試

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 // eslint-disable-next-line no-undef
 export default defineNuxtConfig({
+  ssr: false,
   devtools: { enabled: true },
   css: ['@/assets/main.scss'],
   modules: ['@pinia/nuxt', '@nuxt/content']
